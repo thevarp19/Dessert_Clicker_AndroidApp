b473e3320bae303a213c6768c46261f27060533f
Yerlan Jumabayev <thevarp18@gmail.com>
2023-11-11 11:55:59 +0600

    add dataUistate

--- app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.dessertclicker.data
 
-package com.example.dessertclicker.model
+import com.example.dessertclicker.R
+import com.example.dessertclicker.model.Dessert
 
 /**
- * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ * [Datasource] generates a list of [Dessert]
  */
-data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+object Datasource {
+    val dessertList = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+}
